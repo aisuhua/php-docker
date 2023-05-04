4d293b8d818cd86a936a113d05bf9e3de73494a9
aisuhua <1079087531@qq.com>
2023-05-04 12:32:51 +0800

    优化环境判断逻辑

--- stop.sh
+++ stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f /opt/www/IDC_DEV ]; then
-    IDC_NAME=dev
-else
+if [ -f /opt/www/IDC_LH1 ]; then
     IDC_NAME=lh1
+else
+    IDC_NAME=dev
 fi
 
 IDC_NAME=$IDC_NAME docker-compose down
