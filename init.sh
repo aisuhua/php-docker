675dd82e997b61c86cab7b2c8535cb5fbce13d95
aisuhua <1079087531@qq.com>
2023-05-05 14:08:49 +0800

    将环境变量写在 init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [ -f /opt/www/IDC_LH1 ]; then
-    IDC_NAME=lh1
+    export IDC_NAME=lh1
+    export USERID=1000
+    export GID=0
 else
-    IDC_NAME=dev
+    export IDC_NAME=dev
+    export USERID=1000
+    export GID=0
 fi
-
-export $(grep -v '^#' $(dirname "$0")/conf/"${IDC_NAME}"/env | xargs)
-export IDC_NAME=$IDC_NAME
