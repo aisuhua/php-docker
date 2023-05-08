4788ff2be98c5b4799095b615d52d4defa7f798b
aisuhua <1079087531@qq.com>
2023-05-08 14:44:45 +0800

    加入 sshd 服务

--- php80/docker-entrypoint.sh
+++ php80/docker-entrypoint.sh
@@ -3,4 +3,16 @@
 
 set -e
 
+cp /etc/passwd /tmp/passwd
+sed -i -e "s/1001/`id -u`/g" /tmp/passwd
+cat /tmp/passwd > /etc/passwd
+rm /tmp/passwd
+mkdir -p /home/random/.ssh
+cp /ssh/* /home/random/.ssh/
+chmod 700 /home/random/.ssh
+chmod 600 /home/random/.ssh/id_rsa /home/random/.ssh/authorized_keys
+ssh-keygen -y -f /home/random/.ssh/id_rsa > /home/random/.ssh/id_rsa.pub
+ssh-keygen -A > /dev/null
+/usr/sbin/sshd
+
 exec "$@"
